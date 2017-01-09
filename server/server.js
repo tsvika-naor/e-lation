49f9ceee6be7bbb6b88076fdc11826415a10d895
Itamar Priel <itamar.priel@gmail.com>
2017-01-09 22:01:31 +0200

    Server.js patch: Added MongoDB connection info Cleaned old code from previous project

--- server/server.js
+++ server/server.js
@@ -2,17 +2,15 @@
 var express = require('express');
 var app     = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+
+mongoose.connect('mongodb://heroku_7h2r3npw:s4i8a900gf6p41bt1me1gru0u6@ds151228.mlab.com:51228/heroku_7h2r3npw');
 var db = mongoose.connection;
+
 require('./routes/index')(app);
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/www'));
-
-// Initialize auth
-//var auth = require('./modules/googleIdTokenAuth')(pool);
-//var auth = require('./modules/googleIdTokenAuth-Mock');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
