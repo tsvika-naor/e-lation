e4362b35090d51ff0d74dbd449cc41ef2348dd1f
Itamar Priel <itamar.priel@gmail.com>
2017-01-15 13:34:47 +0200

    Fixed routing bug on refresh - Removed Express.static setting - Modified base routes to seperate:   - navigation related routes (html pages)   - asset related routes (js, css, etc.)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,17 @@
+var path = require('path');
 var userRoutes = require('./user.route');
 
-module.exports = function(app) {
-    app.use('/user', userRoutes);
+module.exports = function(app, rootDir) {
+    app.use('/api/user', userRoutes);
+    
+    // Catch all requests for assets (js, css, etc.)
+    app.get('*.*$', function(req, res) {
+        console.log(req.path);
+        res.sendFile(path.join(rootDir, 'www', req.path));
+    });
+
+    // Catch all other routes and return the index file
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(rootDir, 'www', 'index.html'));
+    });
 };
--- server/server.js
+++ server/server.js
@@ -6,11 +6,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://heroku_7h2r3npw:s4i8a900gf6p41bt1me1gru0u6@ds151228.mlab.com:51228/heroku_7h2r3npw');
 var db = mongoose.connection;
 
-require('./routes/index')(app);
+require('./routes/index')(app, __dirname);
 
 app.set('port', process.env.PORT || 5000);
-
-app.use(express.static(__dirname + '/www'));
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
